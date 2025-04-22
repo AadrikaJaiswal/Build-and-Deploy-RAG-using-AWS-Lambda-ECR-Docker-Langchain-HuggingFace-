0f07fd476cfcd58c1c0b199556d83da7635facec
AadrikaJaiswal <iaadrikajaiswal@gmail.com>
2025-04-22 14:11:31 +0530

    code file updated

--- main.py
+++ main.py
@@ -12,20 +12,29 @@ prompt= """
 bedrock= boto3.client(service_name= "bedrock-runtime")
 
 payload= {
-
-
+    "prompt": "[INST]"+prompt+"[/INST]",
+    #"max_gen_len": 512,
+    "temperature": 0.3,
+    "top_p": 0.9
 }
 
 body= json.dumps(payload)
-model_id= "meta.llama3-3-70b-instruct-v1:0"
+model_id= "mistral.mixtral-8x7b-instruct-v0:1"
 
 response= bedrock.invoke_model(
     body= body,
-    model_id= model_id,
+    modelId= model_id,
     accept= "application/json",
-    content_type= "application/json"
+    contentType= "application/json"
 )
 
-response_body= json.loads(response.get("body").read())
-response_text= response_body["generation"]
+# response_body= json.loads(response.get("body").read())
+# response_text= response_body["generation"]
+# print(response_text)
+
+response_body = json.loads(response.get("body").read())
+print(json.dumps(response_body, indent=2))  # 👈 See the full response format
+
+# Then extract the text using the correct key
+response_text = response_body["outputs"][0]["text"]
 print(response_text)
